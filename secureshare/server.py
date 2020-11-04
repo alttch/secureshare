43d956f7255c5a80ceae6efa2d8fb5ca974fb9d7
Sergei S <div@altertech.com>
2020-11-04 04:31:10 +0100

    0.0.10

--- secureshare/server.py
+++ secureshare/server.py
@@ -117,6 +117,8 @@ def download(file_id, file_key, file_name):
         abort(403)
     if delete or f['oneshot']:
         db.query('stor.delete', id=file_id)
+    if delete:
+        return ok()
     response = make_response(contents)
     response.headers['Content-Type'] = f['mimetype']
     response.headers['x-hash-sha256'] = f['sha256sum']
